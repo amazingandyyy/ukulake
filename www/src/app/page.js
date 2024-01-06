1ad9e6b7570dcccf43e95fe11a146d7d37a9f1a5
Andy Chen <amazingandyyy@gmail.com>
2024-01-05 18:29:25 -0800

    styles: fix by standard

--- www/src/app/page.js
+++ www/src/app/page.js
@@ -12,9 +12,16 @@ export default function Songs () {
   }, [])
   if (!data) return <p className='text-center m-8'>Loading...</p>
 
-  return (<div>{
-    data.map(song => (<div key={song.title}>
-      <a href={`/song/${encodeURIComponent(song.title)}`}>{song.title}</a>
-    </div>))
-  }</div>)
+  return (
+    <div>{
+    data.map(song => {
+      return (
+        <div key={song.title}>
+          <a href={`/song/${encodeURIComponent(song.title)}`}>{song.title}</a>
+        </div>
+      )
+    })
+}
+    </div>
+  )
 }
