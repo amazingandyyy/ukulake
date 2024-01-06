1e537cb2dd5004b7dbe0176584dcc6e8c38476ea
Andy Chen <amazingandyyy@gmail.com>
2024-01-05 18:22:27 -0800

    feat: create search feature for v3

--- www/src/app/data/songs.js
+++ www/src/app/data/songs.js
@@ -3,7 +3,6 @@ import { VidioSourceType, TabSourceType } from '@/data/types'
 const songs = [
   {
     title: "Can't Help Falling in Love",
-    original_author: 'Elvis Presley',
     chords: [],
     tab: {
       url: "https://sanjoseukeclub.org/Song%20Book/I%20Can't%20Help%20Falling%20in%20Love%20With%20You%20(Key%20of%20C)%20v2%20web.pdf",
