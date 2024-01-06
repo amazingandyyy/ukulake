71106a6830cde3446a95125e75884d4ea2a9e3c8
Andy Chen <amazingandyyy@gmail.com>
2024-01-05 22:03:50 -0800

    feat: use search store and search for video alternatives

--- www/src/app/stores/index.js
+++ www/src/app/stores/index.js
@@ -1,10 +1,35 @@
 import { create } from 'zustand'
 
-const useLakeStore = create((set, get) => ({
-  lake: [],
-  update: (data) => {
-    set({ lake: data })
-  }
+const useSearchStore = create((set, get) => ({
+  input: "",
+  song: {},
+  videoSrc: '',
+  tabSrc: '',
+  videoOptions: [],
+  tabOptions: [],
+  updateInput: (title) => {
+    set({input: title})
+    fetch(`/api/songs?title=${title}`)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data)
+        set({
+          videoOptions: data.videoSearch,
+          song: data.island,
+          videoSrc: data.island.video.url,
+          tabSrc: data.island.tab.url,
+        })
+      })
+  },
+  setSong: (song) => {
+    set({ song })
+  },
+  setVideoSrc: (url) => {
+    set({ songSrc: url })
+  },
+  setTabSrc: (song) => {
+    set({ tabSrc: url })
+  },
 }))
 
-export { useLakeStore }
+export { useSearchStore }
