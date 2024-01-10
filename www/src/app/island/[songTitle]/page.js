4eec1f20c29b3cb15c18532075684ffa338fd5a1
Andy Chen <amazingandyyy@gmail.com>
2024-01-10 09:15:25 -0800

    refact: header for mobile

--- www/src/app/island/[songTitle]/page.js
+++ www/src/app/island/[songTitle]/page.js
@@ -105,11 +105,11 @@ export default function Page ({ params }) {
           <div className='hover:opacity-70 self-start font-semibold text-2xl'>
             <span className='bg-teal-600 text-white rounded-lg'>˙ᵕ˙</span>
             <span className='pl-1 text-gray-900 inline-block'>Ukulake</span>
-            <span className='pl-1 text-gray-900 font-light'>Island</span>
+            <span className='pl-1 hidden md:inline-block text-gray-900 font-light'>Island</span>
           </div>
         </a>
-        <div className='ml-2 flex-1 p-2 rounded-xl m-1 flex items-center font-bold'>
-          {/* logo */}
+        <div className='ml-2 flex-1 p-2 rounded-xl m-1 items-center font-bold hidden md:flex'>
+          {/* music */}
           <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24' fill='currentColor' className='w-6 h-6'>
             <path fillRule='evenodd' d='M19.952 1.651a.75.75 0 0 1 .298.599V16.303a3 3 0 0 1-2.176 2.884l-1.32.377a2.553 2.553 0 1 1-1.403-4.909l2.311-.66a1.5 1.5 0 0 0 1.088-1.442V6.994l-9 2.572v9.737a3 3 0 0 1-2.176 2.884l-1.32.377a2.553 2.553 0 1 1-1.402-4.909l2.31-.66a1.5 1.5 0 0 0 1.088-1.442V5.25a.75.75 0 0 1 .544-.721l10.5-3a.75.75 0 0 1 .658.122Z' clipRule='evenodd' />
           </svg>
