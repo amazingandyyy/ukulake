0b98de025b5e244d3a488382675a56aced70c612
Andy Chen <amazingandyyy@gmail.com>
2024-01-14 17:38:00 -0800

    feat: improve api

--- www/src/app/api/search/route.js
+++ www/src/app/api/search/route.js
@@ -6,7 +6,7 @@ export async function GET (request) {
   const { searchParams } = new URL(request.url)
   const q = searchParams.get('q') || ''
 
-  if (q) {
+  if (q.length > 2) {
     const d = songs.sort((a, b) => (a.title.replace(/[^a-zA-Z]+/g, '') > b.title.replace(/[^a-zA-Z0-9]+/g, '')) ? 1 : ((b.title.replace(/[^a-zA-Z0-9]+/g, '') > a.title.replace(/[^a-zA-Z0-9]+/g, '')) ? -1 : 0))
     const result = d.filter(song => song.title.toLowerCase().includes(q.toLowerCase()))
     return NextResponse.json(result)
