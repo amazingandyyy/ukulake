3fd46069f363cf4d9098ade510adbd7384b01291
Andy Chen <amazingandyyy@gmail.com>
2024-01-14 17:23:54 -0800

    styles: update styles in details

--- www/src/app/layout.js
+++ www/src/app/layout.js
@@ -63,7 +63,7 @@ export default function RootLayout ({ children }) {
         <link rel='icon' type='image/png' sizes='32x32' href='/icons/favicon-32x32.png' />
         <link rel='icon' type='image/png' sizes='16x16' href='/icons/favicon-16x16.png' />
         <link rel='manifest' href='/icons/site.webmanifest' />
-        <link rel='mask-icon' href='/icons/safari-pinned-tab.svg' color='#5bbad5' />
+        <link rel='mask-icon' href='/icons/safari-pinned-tab.svg' color='#289488' />
         <meta name='msapplication-TileColor' content='#da532c' />
         <meta name='theme-color' content='#ffffff' />
       </Head>
--- www/src/app/page.js
+++ www/src/app/page.js
@@ -1,6 +1,6 @@
 'use client'
 
-import { useCallback, useEffect, useState } from 'react'
+import { useCallback, useEffect, useState, useRef } from 'react'
 import Link from 'next/link'
 import { debounce } from 'lodash'
 
@@ -70,32 +70,33 @@ const suggestSongs = [
 export default function Songs () {
   const [result, setResult] = useState(null)
   const [searchTerms, setSearchTerms] = useState('')
+  const inputRef = useRef(null)
 
   const debounceSearch = debounce((q) => {
-    setResult(null)
-    fetch(`/api/search?q=${q}`)
-      .then((res) => res.json())
-      .then((data) => {
-        setResult(data)
-      })
+    if (q.length > 2) {
+      setResult(null)
+      fetch(`/api/search?q=${q}`)
+        .then((res) => res.json())
+        .then((data) => {
+          setResult(data)
+        })
+    }
   }, 200)
 
   useEffect(() => {
-    if (searchTerms.length > 2) {
-      debounceSearch(searchTerms)
-    }
+    debounceSearch(searchTerms)
   }, [searchTerms])
 
-  const search = useCallback((v) => {
-    setResult(null)
+  const searchOnClick = useCallback((v) => {
     setSearchTerms(v)
+    inputRef.current.focus()
   })
 
   return (
     <div className='flex flex-col h-screen items-center'>
       <div className='flex flex-col items-center w-full pt-48'>
         <Link href='/'>
-          <div className='hover:opacity-70 font-semibold text-3xl'>
+          <div className='font-semibold text-3xl'>
             <span className='bg-teal-600 text-white rounded-lg'>˙ᵕ˙</span>
             <span className='pl-1 text-gray-900'>Ukulake</span>
             <span className='pl-1 text-gray-900 font-light'>Harbor</span>
@@ -103,50 +104,54 @@ export default function Songs () {
         </Link>
         <div className='flex px-2 w-full flex-col items-center justify-stretch pt-6'>
           <input
-            className='text-lg text-center w-full max-w-sm p-2 md:p-4 rounded-lg bg-gray-100 m-1 focus:outline-none focus:ring-2 focus:ring-teal-600'
+            className='text-lg text-center w-full max-w-md p-4 rounded-full bg-white shadow-2xl shadow-gray-400 focus:shadow-none m-1 focus:outline-none ring-2 focus:ring-teal-600 ring-gray-300'
             type='text'
+            ref={inputRef}
             placeholder='Search Songs in the Ukulake'
             value={searchTerms}
             onChange={(e) => setSearchTerms(e.target.value)}
           />
-          {/* {searchTerms && ( */}
-          <button onClick={() => search('')} disabled={Boolean(!searchTerms)} className='block w-full max-w-sm p-2 text-sm rounded-lg bg-gray-100 m-1 focus:outline-none focus:ring-2 focus:ring-black disabled:bg-white disabled:text-white select-none'>
-            Reset
-          </button>
-          {/* )} */}
+          {searchTerms && (
+            <div onClick={() => searchOnClick('')} disabled={Boolean(!searchTerms)} className='inline-block p-1 px-[10px] hover:bg-teal-700 text-[10px] font-semibold rounded-full shadow-2xl bg-teal-600 active:bg-teal-600 m-1 focus:ring-2 text-white select-none active:outline-none cursor-pointer'>
+              new search
+            </div>
+          )}
         </div>
       </div>
 
       {/* show top 20 beginner friendly songs */}
-      {!searchTerms && <div className='w-full max-w-4xl my-auto'>
-        <div className='flex flex-col items-center justify-center'>
-          <span className='text-sm text-gray-500'>Recommendation from ChatGPT</span>
-        </div>
-        <div className='flex flex-col md:flex-row w-full justify-center pb-12'>
-          {suggestSongs.map((list, index) => {
-            return (
-              <div className='p-2 w-full text-center md:text-left' key={index}>
-                <div className='font-medium'>
-                  {list.title}
-                </div>
-                {list.songs.map(song => (
-                  <div key={song} onClick={(e) => search(e.target.innerText)} className='hover:text-teal-600 active:text-teal-700 text-gray-70 cursor-pointer'>
-                    <span className='text-md opacity-80 block'>{song}</span>
-                  </div>
-                ))}
-              </div>
-            )
-          })}
-        </div>
-      </div>}
-
-      {searchTerms && (result !== null
-        ? (<div className='pt-12'>
+      {!searchTerms &&
+        <div className='w-full max-w-4xl my-auto pt-12'>
           <div className='flex flex-col items-center justify-center'>
-            <span className='text-sm text-gray-500'>{result.length} islands found</span>
+            <span className='text-sm text-gray-400 pb-2'>Beginner friendly recommendations from ChatGPT</span>
           </div>
-          <div className='grid grid-cols-3 grow-rows-8 md:grid-rows-4 p-2 md:p-4 gap-1'>
-            {
+          <div className='flex flex-col md:flex-row w-full justify-center pb-12'>
+            {suggestSongs.map((list, index) => {
+              return (
+                <div className='p-2 w-full text-center md:text-left' key={index}>
+                  <div className='font-medium'>
+                    {list.title}
+                  </div>
+                  {list.songs.map(song => (
+                    <div key={song} onClick={(e) => searchOnClick(e.target.innerText)} className='hover:text-teal-600 active:text-teal-700 text-gray-70 cursor-pointer'>
+                      <span className='text-md opacity-80 block'>{song}</span>
+                    </div>
+                  ))}
+                </div>
+              )
+            })}
+          </div>
+        </div>}
+
+      {searchTerms &&
+      (result !== null
+        ? (
+          <div className='pt-12'>
+            <div className='flex flex-col items-center justify-center'>
+              <span className='text-sm text-gray-500'>{result.length} islands found</span>
+            </div>
+            <div className='grid grid-cols-3 grow-rows-8 md:grid-rows-4 p-2 md:p-4 gap-1'>
+              {
               result.map(song => {
                 return (
                   <a href={`/island/${encodeURIComponent(song.title)}?s=${song.source}`} key={song.tabSrc} className='col-span-3 md:col-span-1 hover:opacity-70 rounded-md p-2 px-4 hover:text-teal-800 bg-gray-50'>
@@ -156,14 +161,17 @@ export default function Songs () {
                 )
               })
             }
+            </div>
           </div>
-        </div>)
-        : (<div className='text-center m-8'>
-          <div className='animate-bounce hover:opacity-70 text-sm flex flex-row'>
-            <img src='/icons/logo.svg' className='w-6 h-auto' />
-            <span className='pl-1 text-gray-900'>searching</span>
-          </div>
-        </div>))}
+          )
+        : (
+          <div className='text-center m-8'>
+            <div className='animate-bounce hover:opacity-70 text-sm flex flex-row'>
+              <img src='/icons/logo.svg' className='w-6 h-auto' />
+              <span className='pl-1 text-gray-900'>searching</span>
+            </div>
+          </div>)
+      )}
     </div>
   )
 }
