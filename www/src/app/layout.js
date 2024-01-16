c18a563d8ed60ee1827302d4e762561c231bcd18
Andy Chen <amazingandyyy@gmail.com>
2024-01-15 21:06:23 -0800

    fix: fix crisp

--- www/src/app/layout.js
+++ www/src/app/layout.js
@@ -53,7 +53,7 @@ export default function RootLayout ({ children }) {
       <Script
         strategy='afterInteractive' id='crisp-1' dangerouslySetInnerHTML={{
           __html: `
-            window.$crisp=[];window.CRISP_WEBSITE_ID="${CRISP_WEBSITE_ID}";(function(){d=document;s=d.createElement("script");s.src="https://client.crisp.chat/l.js";s.async=1;d.getElementsByTagName("head")[0].appendChild(s);})();
+            window.$crisp=[];window.CRISP_WEBSITE_ID="${process.env.NEXT_PUBLIC_CRISP_WEBSITE_ID}";(function(){d=document;s=d.createElement("script");s.src="https://client.crisp.chat/l.js";s.async=1;d.getElementsByTagName("head")[0].appendChild(s);})();
           `
         }}
       />
